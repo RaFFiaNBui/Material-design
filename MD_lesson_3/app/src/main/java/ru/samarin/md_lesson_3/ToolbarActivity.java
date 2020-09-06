667d0edc2eace8b4f57a5defdcd6282553bd4987
RaFFiaNBui <Raffian_bui@mail.ru>
2020-09-06 14:10:06 +0300

    Homework of 1-2 paragraph 3 lesson course Material-Design

---
+++ MD_lesson_3/app/src/main/java/ru/samarin/md_lesson_3/ToolbarActivity.java
@@ -0,0 +1,34 @@
+package ru.samarin.md_lesson_3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class ToolbarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Log.d("Tag", "onOptionsItemSelected: ");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
