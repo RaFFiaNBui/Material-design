b55a53119835f48fea94a6a75e7239ecf0f6cd8b
RaFFiaNBui <Raffian_bui@mail.ru>
2020-09-09 23:47:28 +0300

    Homework of 2 paragraph 5 lesson course Material-Design

---
+++ MD_lesson_3/app/src/main/java/ru/samarin/md_lesson_3/FabActivity.java
@@ -0,0 +1,14 @@
+package ru.samarin.md_lesson_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fab);
+    }
+}
